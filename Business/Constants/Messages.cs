3f17dcb1eb646ef5b2442ac1e87fd0680d9131bb
Gökhan Aydın <aydingo18@itu.edu.tr>
2021-02-13 17:13:39 +0300

    Updated again

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -16,9 +16,22 @@ namespace Business.Constants
         public static string BrandDeleted = "Marka Silindi";
         public static string BrandUpdated = "Marka Güncellendi";
 
-        public static string ColorAdded = "Marka Eklendi";
-        public static string ColorInvalidInput = "Marka Eklenemdi";
-        public static string ColorDeleted = "Marka Silindi";
-        public static string ColorUpdated = "Marka Güncellendi";
+        public static string ColorAdded = "Renk Eklendi";
+        public static string ColorInvalidInput = "Renk Eklenemdi";
+        public static string ColorDeleted = "Renk Silindi";
+        public static string ColorUpdated = "Renk Güncellendi";
+
+        public static string UserAdded = "Kullanıcı Eklendi";
+        public static string UserDeleted = "Kullanıcı Silindi";
+        public static string UserUpdated = "Kullanıcı Güncellendi";
+
+        public static string CustomerAdded = "Müsteri Eklendi";
+        public static string CustomerDeleted = "Müşteri Silindi";
+        public static string CustomerUpdated = "Müşteri Güncellendi";
+
+        public static string RentalAdded = "Kira İşlemi Eklendi";
+        public static string RentalDeleted = "Kira İşlemi Silindi";
+        public static string RentalUpdated = "Kira İşlemi Güncellendi";
+
     }
 }
--- Console/Program.cs
+++ Console/Program.cs
@@ -23,10 +23,75 @@ namespace Console
             //SecondHW(carManager);
 
             ////////////////////////////////////////////////////////////
-            ///
 
-            Dictionary<int, string> colors = new Dictionary<int, string>() 
-            { 
+
+            ThirdHW(carManager, colorManager, brandManager);
+
+            ////////////////////////////////////////////////////////////
+
+            UserManager userManager = new UserManager(new EfUserDal());
+
+            userManager.Add(new User
+            {
+                Id = 1,
+                FirstName = "Ahmet",
+                LastName = "Yılmaz",
+                Email = "ahmetyilmaz@gmail.com",
+                Password = "1234556"
+            });
+
+            userManager.Add(new User
+            {
+                Id = 2,
+                FirstName = "Süleyman",
+                LastName = "Açıkgöz",
+                Email = "süleymanacikgoz@gmail.com",
+                Password = "09876"
+            });
+
+            userManager.Add(new User
+            {
+                Id = 3,
+                FirstName = "Gökhan",
+                LastName = "Aydın",
+                Email = "gokhanaydin@gmail.com",
+                Password = "12345642"
+            });
+
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+
+            customerManager.Add(new Customer
+            {
+                Id = 1,
+                UserId = 1,
+                CompanyName = "sada"
+            });
+
+            customerManager.Add(new Customer
+            {
+                Id = 2,
+                UserId = 2
+            });
+
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+
+            rentalManager.Add(new Rental 
+            {
+                Id = 1,
+                CarId = 2,
+                CustomerId = 1,
+                RentDate = new DateTime(2021, 2, 13),
+                ReturnDate = new DateTime(2021, 2, 13)
+            });
+
+        }
+
+        ///////////////////////////////////////
+        
+        private static void ThirdHW(CarManager carManager, ColorManager colorManager, BrandManager brandManager)
+        {
+            Dictionary<int, string> colors = new Dictionary<int, string>()
+            {
                 [1] = "Mavi",
                 [2] = "Kırmızı",
                 [3] = "Siyah",
@@ -65,7 +130,7 @@ namespace Console
                     Name = brand.Value
                 });
             }
-            
+
 
 
             ///////////////////////////////
@@ -94,93 +159,94 @@ namespace Console
             ///////////////////////////////
 
 
-            carManager.Update(new Car { Id = 2, BrandId = 2, ColorId = 2, CarName = "Mercedes", DailyPrice = 2000, ModelYear = 2014});
+            //    carManager.Update(new Car { Id = 2, BrandId = 2, ColorId = 2, CarName = "Mercedes", DailyPrice = 2000, ModelYear = 2014 });
 
-            System.Console.WriteLine("-------------GetAll----------");
-            foreach (var car in carManager.GetAll())
-            {
-                System.Console.WriteLine(car.CarName);
-            }
-            System.Console.WriteLine("-------------------------------------------");
+            //    System.Console.WriteLine("-------------GetAll----------");
+            //    foreach (var car in carManager.GetAll())
+            //    {
+            //        System.Console.WriteLine(car.CarName);
+            //    }
+            //    System.Console.WriteLine("-------------------------------------------");
 
-            brandManager.Delete(new Brand {Id = 4});
+            //    brandManager.Delete(new Brand { Id = 4 });
 
-            System.Console.WriteLine("-------------GetAllBrands----------");
+            //    System.Console.WriteLine("-------------GetAllBrands----------");
 
-            foreach (var brand in brandManager.GetAll())
-            {
-                System.Console.WriteLine(brand.Name);
-            }
-            System.Console.WriteLine("-------------------------------------------");
+            //    foreach (var brand in brandManager.GetAll())
+            //    {
+            //        System.Console.WriteLine(brand.Name);
+            //    }
+            //    System.Console.WriteLine("-------------------------------------------");
 
 
-            System.Console.WriteLine("-------------GetCarDetails----------");
+            //    System.Console.WriteLine("-------------GetCarDetails----------");
 
-            foreach (var car in carManager.GetCarDetails())
-            {
-                System.Console.WriteLine("Araba ismi: {0}, Markası: {1}, Rengi: {2}, Günlük Ücreti: {3}", car.CarName, car.BrandName, car.ColorName, car.DailyPrice);
-            }
+            //    foreach (var car in carManager.GetCarDetails())
+            //    {
+            //        System.Console.WriteLine("Araba ismi: {0}, Markası: {1}, Rengi: {2}, Günlük Ücreti: {3}", car.CarName, car.BrandName, car.ColorName, car.DailyPrice);
+            //    }
+            //}
+
+            //private static void SecondHW(CarManager carManager)
+            //{
+            //    // Hata Mesajı Verir.
+            //    carManager.Add(new Car
+            //    {
+            //        BrandId = 2,
+            //        ColorId = 3,
+            //        CarName = "W",
+            //        ModelYear = 2010,
+            //        DailyPrice = 1000,
+
+            //    });
+
+            //    // Başarılı bir şekilde ekleme yapar.
+            //    carManager.Add(new Car
+            //    {
+            //        BrandId = 2,
+            //        ColorId = 3,
+            //        CarName = "BMW",
+            //        ModelYear = 2010,
+            //        DailyPrice = 1000,
+
+            //    });
+
+            //    foreach (var car in carManager.GetAll())
+            //    {
+            //        System.Console.WriteLine(car.CarName);
+            //    }
+            //    System.Console.WriteLine("------------------");
+            //}
+
+            //private static void FirstHW(CarManager carManager)
+            //{
+            //    foreach (var car in carManager.GetAll())
+            //    {
+            //        System.Console.WriteLine("Araba ID: " + car.Id + " " +
+            //                                "Araba Modeli: " + car.Description + " " +
+            //                                "Araba Marka ID: " + car.BrandId + " " +
+            //                                "Araba Renk ID: " + car.ColorId + " " +
+            //                                "Araba Günlük Ücreti: " + car.DailyPrice);
+            //    }
+            //    System.Console.WriteLine("-----------------------\n");
+
+            //    System.Console.WriteLine("Get By ID");
+
+            //    System.Console.WriteLine("------------");
+
+            //    foreach (var car1 in carManager.GetById(2))
+            //    {
+            //        System.Console.WriteLine("Araba ID: " + car1.Id + " " + "Araba Modeli: " + car1.Description);
+            //    }
+
+            //    for (int ID = 1; ID <= carManager.GetAll().Count; ID++)
+            //    {
+            //        foreach (var car1 in carManager.GetById(ID))
+            //        {
+            //            System.Console.WriteLine("Araba ID: " + car1.Id + " " + "Araba Modeli: " + car1.Description);
+            //        }
+            //    }
+            //}
         }
-
-        //private static void SecondHW(CarManager carManager)
-        //{
-        //    // Hata Mesajı Verir.
-        //    carManager.Add(new Car
-        //    {
-        //        BrandId = 2,
-        //        ColorId = 3,
-        //        CarName = "W",
-        //        ModelYear = 2010,
-        //        DailyPrice = 1000,
-
-        //    });
-
-        //    // Başarılı bir şekilde ekleme yapar.
-        //    carManager.Add(new Car
-        //    {
-        //        BrandId = 2,
-        //        ColorId = 3,
-        //        CarName = "BMW",
-        //        ModelYear = 2010,
-        //        DailyPrice = 1000,
-
-        //    });
-
-        //    foreach (var car in carManager.GetAll())
-        //    {
-        //        System.Console.WriteLine(car.CarName);
-        //    }
-        //    System.Console.WriteLine("------------------");
-        //}
-
-        //private static void FirstHW(CarManager carManager)
-        //{
-        //    foreach (var car in carManager.GetAll())
-        //    {
-        //        System.Console.WriteLine("Araba ID: " + car.Id + " " +
-        //                                "Araba Modeli: " + car.Description + " " +
-        //                                "Araba Marka ID: " + car.BrandId + " " +
-        //                                "Araba Renk ID: " + car.ColorId + " " +
-        //                                "Araba Günlük Ücreti: " + car.DailyPrice);
-        //    }
-        //    System.Console.WriteLine("-----------------------\n");
-
-        //    System.Console.WriteLine("Get By ID");
-
-        //    System.Console.WriteLine("------------");
-
-        //    foreach (var car1 in carManager.GetById(2))
-        //    {
-        //        System.Console.WriteLine("Araba ID: " + car1.Id + " " + "Araba Modeli: " + car1.Description);
-        //    }
-
-        //    for (int ID = 1; ID <= carManager.GetAll().Count; ID++)
-        //    {
-        //        foreach (var car1 in carManager.GetById(ID))
-        //        {
-        //            System.Console.WriteLine("Araba ID: " + car1.Id + " " + "Araba Modeli: " + car1.Description);
-        //        }
-        //    }
-        //}
     }
 }
